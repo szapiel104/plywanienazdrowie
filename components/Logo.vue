459ba2f6fc5395868484909cb66ed00bf72dd4cb
Michał Szapiel <szapiel104@gmail.com>
2021-06-07 22:43:43 +0200

    Initial commit

---
+++ components/Logo.vue
@@ -0,0 +1,17 @@
+<template>
+  <img class="NuxtLogo" src="../static/img/logo/alpakilodzkie_logo_01.png" />
+</template>
+
+<style>
+.NuxtLogo {
+  animation: 1s appear;
+  margin: auto;
+  max-width: 200px;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
