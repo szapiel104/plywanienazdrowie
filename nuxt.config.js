09553acb81c740d20aaca4b01e8bcfee33a578f3
Michał Szapiel <szapiel104@gmail.com>
2021-08-30 21:18:41 +0200

    Google maps :: alternative

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,9 +20,6 @@ export default {
     '@/assets/sass/main.scss',
   ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -34,9 +31,18 @@ export default {
   modules: [
     // Doc: https://bootstrap-vue.js.org/docs/
     "bootstrap-vue/nuxt",
+    // Simple usage
+    'nuxt-leaflet',
+
+    // With options
+    ['nuxt-leaflet', { /* module options */ }],
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+    vendor: ["vue2-google-maps"]
+  },
+  plugins: [
+    "@/plugins/vue2-google-maps.js"
+  ],
 }
--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,16 @@
     <div class="row my-5 d-flex justify">
       <div class="col-12 col-md-6 my-auto">
         <h1>Pływanie na zdrowie</h1>
-        <p class="content-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fringilla leo eu sapien aliquam, hendrerit facilisis libero pretium.</p>
+        <p class="content-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fringilla leo eu sapien
+          aliquam, hendrerit facilisis libero pretium.</p>
         <div class="row">
           <div class="col-12 col-md-4">
             <b-button size="sm" class="my-2 a-link-btn normal" type="primary" variant="primary" to="/harmonogram">
               <span class="mr-1">Oferta</span>
-              <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
+              <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor"
+                   class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                      d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
               </svg>
             </b-button>
           </div>
@@ -22,19 +25,44 @@
 
       </div>
       <div class="col-12 col-md-6">
-        <img src="~assets/img/erik-dungan-lNUi7W4iwok-unsplash.jpg" alt="" class="img-fluid" style="border-radius: 10px">
+        <img src="~assets/img/erik-dungan-lNUi7W4iwok-unsplash.jpg" alt="" class="img-fluid"
+             style="border-radius: 10px">
       </div>
     </div>
-    <p class="my-5">W trakcie pracy ... </p>
-    <p class="my-5">W trakcie pracy ... </p>
-    <p class="my-5">W trakcie pracy ... </p>
-    <p class="my-5">W trakcie pracy ... </p>
-    <p class="my-5">W trakcie pracy ... </p>
+    <div class="row my-5 d-flex justify">
+      <div class="col-12">
+        <h1 class="mb-3">Znajdź nas</h1>
+        <div id="map-wrap" style="height: 600px">
+          <no-ssr>
+            <l-map :zoom=13 :center="[51.7034009,19.4283212]">
+              <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+              <l-marker :lat-lng="[51.7034009,19.4283212]"><l-popup><a href="#">Lokalizacja (link do google maps)</a></l-popup></l-marker>
+              <l-marker :lat-lng="[51.6934001,19.4283212]"><l-popup><a href="#">Lokalizacja (link do google maps)</a></l-popup></l-marker>
+            </l-map>
+          </no-ssr>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+
+
 export default {
+  data() {
+    return {
+      // center: {lat: 51.6054142, lng: 19.4996098},
+      // markers:[
+      //   {
+      //     position: {lat: 10.0, lng: 10.0}
+      //   },
+      //   {
+      //     position: {lat: 10.0, lng: 10.0}
+      //   }
+      // ]
+    }
+  },
   head() {
     return {
       title: 'Pływanie na zdrowie',
@@ -48,6 +76,12 @@ export default {
         }
       ],
     }
-  }
+  },
+
+
 }
 </script>
+<style>
+.leaflet-popup-content-wrapper{
+}
+</style>
