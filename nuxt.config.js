84fa082f0cc03b46d981ff7ffdccf5256c4e3be0
Michał Szapiel <szapiel104@gmail.com>
2021-11-19 17:21:53 +0100

    No index

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
+      { hid: 'robots', name: 'robots', content: 'noindex, nofollow' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
